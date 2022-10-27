bd0a27c5f5c2470a2b47db78f34bbe09ae9b9de6
astashovmv <astashovmv1996@gmail.com>
2022-10-27 20:55:11 +0500

    pp
    pp

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,108 +1,100 @@
 package jm.task.core.jdbc.dao;
-
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getConnection;
+
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
-    public  void createUsersTable() {
+    public void createUsersTable() throws SQLException {
         PreparedStatement preparedStatement = null;
-        User user = new User("Maxim", "Astashov", (byte)26);
-        //user.setId(1L);
-        //String sql = "insert into users (id, name, lastName, age) values(?, ?, ?, ?)";
-        String sql = "insert into users (name, lastName, age) values(?, ?, ?)";
-        Util util = new Util();
+        String sql = "insert into users (name, lastName, age) values('Maxim', 'Astashov', 26)";
         try {
-            preparedStatement = util.getConnection().prepareStatement(sql);
-            //preparedStatement.setLong(1, user.getId());
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setByte(3, user.getAge());
-
+            preparedStatement = connection.prepareStatement(sql);
             preparedStatement.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("connection error");
+            connection.rollback();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
         }
-        dropUsersTable();
+        cleanUsersTable();
     }
 
-    public void dropUsersTable() {
-        PreparedStatement preparedStatement = null;
-        String sql = "delete from users";
-        Util util = new Util();
-        try {
-            preparedStatement = util.getConnection().prepareStatement(sql);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("connection error");
-        }
+    public void dropUsersTable() throws SQLException {
+        List<User> userListDelete = getAllUsers();
+        userListDelete.clear();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-        User user = new User();
-        user.setName(name);
-        user.setLastName(lastName);
-        user.setAge(age);
-
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         PreparedStatement preparedStatement = null;
         String sql = "insert into users (name, lastName, age) values(?, ?, ?)";
-        Util util = new Util();
         try {
-            preparedStatement = util.getConnection().prepareStatement(sql);
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setByte(3, user.getAge());
-
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("connection error");
+            connection.rollback();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
         }
-
         System.out.println("User с именем – " + name + " добавлен в базу данных");
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
         PreparedStatement preparedStatement = null;
-        Util util = new Util();
-        User user = new User("Maxim", "Astashov", (byte)26);
-        user.setId(1L);
         String sql = "delete from users where id = ?";
 
         try {
-            preparedStatement = util.getConnection().prepareStatement(sql);
-            preparedStatement.setLong(1, user.getId());
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
 
             preparedStatement.executeUpdate();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("connection error");
+            connection.rollback();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
         }
     }
 
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() throws SQLException {
         List<User> userList = new ArrayList<>();
 
         String sql = "select id, name, lastName, age from users";
 
-        try {
-            Util util = new Util();
-            Statement statement = null;
+        Statement statement = null;
 
-            statement = util.getConnection().createStatement();
+        try {
+
+            statement = connection.createStatement();
 
             ResultSet resultSet = statement.executeQuery(sql);
-
+            connection.commit();
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("id"));
@@ -115,14 +107,32 @@ public class UserDaoJDBCImpl implements UserDao {
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("connection error");
+            connection.rollback();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
         }
         return userList;
     }
 
-    public void cleanUsersTable() {
-        List<User> userListDelete = getAllUsers();
-        userListDelete.clear();
-        dropUsersTable();
+    public void cleanUsersTable() throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String sql = "delete from users";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("connection error");
+            connection.rollback();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
         }
     }
+
+}
 
